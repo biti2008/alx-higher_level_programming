348f6d00a0239c10672c2607653fe3906c560f6e
biti2008 <tesfaybrkti00@gmail.com>
2022-05-31 14:48:03 -0700

    My num six commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if i == 8 and j == 9:
+for i in range(0, 10):
+    for j in range(1, 10):
+        if i >= j:
+            continue
+        elif i == 8 and j == 9:
             print("{}{}".format(i, j))
-        elif i != j and j > i:
+        else:
             print("{}{}, ".format(i, j), end="")

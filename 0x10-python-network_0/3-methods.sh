e51b48336950bf15c9bce929c09eb9be2a0785da
Birktiy Tesfay <Birktiy.Tesfay@safaricom.et>
2022-09-22 11:43:29 -0700

    My commit

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
-
-[A[A[B#!/bin/bash
+#!/bin/bash
 # This script takes in a URL and displays all HTTP methods the server will accept.
 curl -sI "$1" -X OPTIONS | grep "Allow:" | cut -d':' -f2 | sed 's/ //'
